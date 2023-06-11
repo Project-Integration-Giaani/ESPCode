7208f85a8203b427dbe4f4410d91d4806bb36f1a
Mexigore <axelc2000@live.com.mx>
2023-06-11 20:56:54 +0200

    Emergency base code and organized code

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,8 @@
 
 #include <map>
 
+#define EMERGENCY_PIN 32
+
 // Wifi credentials 
 // #define WIFI_SSID "Minkmates"
 // #define WIFI_PASS "Minkmaatstraat50"
@@ -88,6 +90,10 @@ const int   daylightOffset_sec = 3600;
 // char SQLuser[] = "nesnos";              // MySQL user login username
 // char password[] = "Axeltalksalot3!";        // MySQL user login password
 
+/*<------------------------->
+  <----GLOBAL VARIABLES ---->
+  <------------------------->*/
+
 
 typedef struct
 { // struct for the std::map below
@@ -101,7 +107,7 @@ typedef struct
 // right now we have 4 devicesIds going to 4 relays and 4 flip switches to switch the relay manually
 std::map<String, deviceConfig_t> devices = {
 	//{deviceId, {relayPIN,  flipSwitchPIN}}
-	{"645e4cd6929949c1da656545", {32, 17}}};
+	{"645e4cd6929949c1da656545", {EMERGENCY_PIN, 17}}};
 
 typedef struct
 { // struct for the std::map below
@@ -112,6 +118,38 @@ typedef struct
 
 std::map<int, flipSwitchConfig_t> flipSwitches; // this map is used to map flipSwitch PINs to deviceId and handling debounce and last flipSwitch state checks
 												// it will be setup in "setupFlipSwitches" function, using informations from devices map
+
+
+/*<---------------------------->
+  <----- SET UP FUNCTIONS ----->
+  <---------------------------->*/
+
+void setupWiFi()
+{
+	Serial.printf("\r\n[Wifi]: Connecting");
+	WiFi.begin(WIFI_SSID, WIFI_PASS);
+	
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.printf(".");
+		delay(250);
+	}
+	//digitalWrite(LED_BUILTIN, HIGH);
+	Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+void setupSinricPro()
+{
+	for (auto &device : devices)
+	{
+		const char *deviceId = device.first.c_str();
+		SinricProSwitch &mySwitch = SinricPro[deviceId];
+		mySwitch.onPowerState(onPowerState);
+	}
+
+	SinricPro.begin(APP_KEY, APP_SECRET);
+	SinricPro.restoreDeviceStates(true);
+}
 
 void setupRelays()
 {
@@ -139,6 +177,36 @@ void setupFlipSwitches()
 	}
 }
 
+void setup_tft(){
+	tft.init();
+	tft.fillScreen(TFT_RED);
+	tft.setRotation(1);
+	tft.setTextWrap(true,true);
+}
+
+void time_setup(){
+	// Init and get the time
+	configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+	printLocalTime();
+
+}
+void setupGoogleHomeNotifier() {
+  const char deviceName[] = "Office";
+
+  Serial.println("connecting to Google Home...");
+  if (ghn.device(deviceName, "en") != true) {
+    Serial.println(ghn.getLastError());
+    return;
+  }
+
+  Serial.print("found Google Home(");
+  Serial.print(ghn.getIPAddress());
+  Serial.print(":");
+  Serial.print(ghn.getPort());
+  Serial.println(")");
+
+}
+
 // void setupSQL()
 // {
 // 	// if (!WiFi.hostByName("giaani-mysql.mysql.database.azure.com", server_addr)) {
@@ -152,6 +220,76 @@ void setupFlipSwitches()
 // 		Serial.println("Connection to SQL Server failed.");
 // 	}
 // }
+
+/*<--------------------------->
+  <------ USE FUNCTIONS ------>
+  <--------------------------->*/
+
+void GoogleHomeMessage(const char* message){
+	if (ghn.notify(message) != true) {
+    Serial.println(ghn.getLastError());
+    return;
+  }
+  Serial.println("Google Home Notifier meesage sent sccessfully.");
+}
+ 
+void emergency() {
+	//send to database emergency
+	GoogleHomeMessage("Emergency detected, notifying nurse");
+}
+
+bool onPowerState(String deviceId, bool &state)
+{
+	Serial.printf("%s: %s\r\n", deviceId.c_str(), state ? "on" : "off");
+	int relayPIN = devices[deviceId].relayPIN; // get the relay pin for corresponding device
+	digitalWrite(relayPIN, !state);			   // set the new relay state
+
+	if (relayPIN == EMERGENCY_PIN && state == true) {
+		emergency();
+	}
+	
+	return true;
+}
+
+/*<-------------------------->
+  <----- LOOP FUNCTIONS ----->
+  <-------------------------->*/
+
+void handleFlipSwitches()
+{
+	unsigned long actualMillis = millis(); // get actual millis
+	for (auto &flipSwitch : flipSwitches)
+	{																				 // for each flipSwitch in flipSwitches map
+		unsigned long lastFlipSwitchChange = flipSwitch.second.lastFlipSwitchChange; // get the timestamp when flipSwitch was pressed last time (used to debounce / limit events)
+
+		if (actualMillis - lastFlipSwitchChange > DEBOUNCE_TIME)
+		{ // if time is > debounce time...
+
+			int flipSwitchPIN = flipSwitch.first;							  // get the flipSwitch pin from configuration
+			bool lastFlipSwitchState = flipSwitch.second.lastFlipSwitchState; // get the lastFlipSwitchState
+			bool flipSwitchState = digitalRead(flipSwitchPIN);				  // read the current flipSwitch state
+			if (flipSwitchState != lastFlipSwitchState)
+			{ // if the flipSwitchState has changed...
+#ifdef TACTILE_BUTTON
+				if (flipSwitchState)
+				{ // if the tactile button is pressed
+#endif
+					flipSwitch.second.lastFlipSwitchChange = actualMillis; // update lastFlipSwitchChange time
+					String deviceId = flipSwitch.second.deviceId;		   // get the deviceId from config
+					int relayPIN = devices[deviceId].relayPIN;			   // get the relayPIN from config
+					bool newRelayState = !digitalRead(relayPIN);		   // set the new relay State
+					digitalWrite(relayPIN, newRelayState);				   // set the trelay to the new state
+
+					SinricProSwitch &mySwitch = SinricPro[deviceId]; // get Switch device from SinricPro
+					mySwitch.sendPowerStateEvent(!newRelayState);	 // send the event
+#ifdef TACTILE_BUTTON
+				}
+#endif
+				flipSwitch.second.lastFlipSwitchState = flipSwitchState; // update lastFlipSwitchState
+			}
+		}
+	}
+}
 
 void printLocalTime() {
 	struct tm timeinfo;
@@ -205,116 +343,6 @@ void printLocalTime() {
 			alarm_set = true;
 		}
 	}
-}
-
-void setup_tft(){
-	tft.init();
-	tft.fillScreen(TFT_RED);
-	tft.setRotation(1);
-	tft.setTextWrap(true,true);
-}
-
-void time_setup(){
-	// Init and get the time
-	configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-	printLocalTime();
-
-}
-void setupGoogleHomeNotifier() {
-  const char deviceName[] = "Office";
-
-  Serial.println("connecting to Google Home...");
-  if (ghn.device(deviceName, "en") != true) {
-    Serial.println(ghn.getLastError());
-    return;
-  }
-
-  Serial.print("found Google Home(");
-  Serial.print(ghn.getIPAddress());
-  Serial.print(":");
-  Serial.print(ghn.getPort());
-  Serial.println(")");
-
-}
-
-void GoogleHomeMessage(const char* message){
-	if (ghn.notify(message) != true) {
-    Serial.println(ghn.getLastError());
-    return;
-  }
-  Serial.println("Google Home Notifier meesage sent sccessfully.");
-}
- 
-bool onPowerState(String deviceId, bool &state)
-{
-	Serial.printf("%s: %s\r\n", deviceId.c_str(), state ? "on" : "off");
-	int relayPIN = devices[deviceId].relayPIN; // get the relay pin for corresponding device
-	digitalWrite(relayPIN, !state);			   // set the new relay state
-	
-	return true;
-}
-
-void handleFlipSwitches()
-{
-	unsigned long actualMillis = millis(); // get actual millis
-	for (auto &flipSwitch : flipSwitches)
-	{																				 // for each flipSwitch in flipSwitches map
-		unsigned long lastFlipSwitchChange = flipSwitch.second.lastFlipSwitchChange; // get the timestamp when flipSwitch was pressed last time (used to debounce / limit events)
-
-		if (actualMillis - lastFlipSwitchChange > DEBOUNCE_TIME)
-		{ // if time is > debounce time...
-
-			int flipSwitchPIN = flipSwitch.first;							  // get the flipSwitch pin from configuration
-			bool lastFlipSwitchState = flipSwitch.second.lastFlipSwitchState; // get the lastFlipSwitchState
-			bool flipSwitchState = digitalRead(flipSwitchPIN);				  // read the current flipSwitch state
-			if (flipSwitchState != lastFlipSwitchState)
-			{ // if the flipSwitchState has changed...
-#ifdef TACTILE_BUTTON
-				if (flipSwitchState)
-				{ // if the tactile button is pressed
-#endif
-					flipSwitch.second.lastFlipSwitchChange = actualMillis; // update lastFlipSwitchChange time
-					String deviceId = flipSwitch.second.deviceId;		   // get the deviceId from config
-					int relayPIN = devices[deviceId].relayPIN;			   // get the relayPIN from config
-					bool newRelayState = !digitalRead(relayPIN);		   // set the new relay State
-					digitalWrite(relayPIN, newRelayState);				   // set the trelay to the new state
-
-					SinricProSwitch &mySwitch = SinricPro[deviceId]; // get Switch device from SinricPro
-					mySwitch.sendPowerStateEvent(!newRelayState);	 // send the event
-#ifdef TACTILE_BUTTON
-				}
-#endif
-				flipSwitch.second.lastFlipSwitchState = flipSwitchState; // update lastFlipSwitchState
-			}
-		}
-	}
-}
-
-void setupWiFi()
-{
-	Serial.printf("\r\n[Wifi]: Connecting");
-	WiFi.begin(WIFI_SSID, WIFI_PASS);
-	
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		Serial.printf(".");
-		delay(250);
-	}
-	//digitalWrite(LED_BUILTIN, HIGH);
-	Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
-}
-
-void setupSinricPro()
-{
-	for (auto &device : devices)
-	{
-		const char *deviceId = device.first.c_str();
-		SinricProSwitch &mySwitch = SinricPro[deviceId];
-		mySwitch.onPowerState(onPowerState);
-	}
-
-	SinricPro.begin(APP_KEY, APP_SECRET);
-	SinricPro.restoreDeviceStates(true);
 }
 
 void setup()
